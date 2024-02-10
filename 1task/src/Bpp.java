d1cadb336bc690247e8825e8882fdfd52fcf92eb
raiL0ng <19nik.nikitov.97@mail.ru>
2024-02-10 15:25:47 +0400

    Edit B-task

--- 1task/src/Bpp.java
+++ 1task/src/Bpp.java
@@ -6,38 +6,40 @@ public class Bpp {
     public static void main(String[] args) throws Exception {
         Scanner in = new Scanner(System.in);
         ArrayList<BigDecimal> ans = new ArrayList<>();
-        while (in.hasNextLine()) {
-            Double v = in.nextDouble();
-            Double u = in.nextDouble();
-            String oper = in.nextLine();
-            BigDecimal a = new BigDecimal(v);
-            BigDecimal b = new BigDecimal(u);
-            if (oper.contains("ADD")) {
-                ans.add(a.add(b));
-            } else if (oper.contains("SUB")) {
-                ans.add(a.subtract(b));
-            } else if (oper.contains("MULT")) {
-                ans.add(a.multiply(b));
-            } else if (oper.contains("DIV")) {
-                if (b.equals(BigDecimal.ZERO)) {
-                    System.out.println("None: (деление на ноль!)");
-                    System.exit(0);
+        try {
+            while (in.hasNextLine()) {
+                Double v = in.nextDouble();
+                Double u = in.nextDouble();
+                String oper = in.nextLine();
+                BigDecimal a = new BigDecimal(v);
+                BigDecimal b = new BigDecimal(u);
+                if (oper.length() == 0) {
+                    break;
                 }
-                else {
+                if (oper.contains("ADD")) {
+                    ans.add(a.add(b));
+                } else if (oper.contains("SUB")) {
+                    ans.add(a.subtract(b));
+                } else if (oper.contains("MULT")) {
+                    ans.add(a.multiply(b));
+                } else if (oper.contains("DIV")) {
                     ans.add(a.divide(b));
+                } else if (oper.contains("REM")) {
+                    ans.add(a.remainder(b));
+                } else if (oper.contains("POW")) {
+                    ans.add(a.pow(Integer.parseInt(b.toString())));
                 }
-            } else if (oper.contains("REM")) {
-                ans.add(a.remainder(b));
-            } else if (oper.contains("POW")) {
-                ans.add(a.pow(Integer.parseInt(b.toString())));
-            } else {
-                System.out.println("None");
-                System.exit(0);
             }
+        } catch (Exception e) {
+            System.out.printf("Некорректный ввод данных!\n(%d)\n", e.getMessage());
+            System.exit(0);
         }
-        for (BigDecimal el : ans) {
-            System.out.println(el);
+        finally {
+            for (BigDecimal el : ans) {
+                System.out.println(el);
+            }    
         }
+
         in.close();
     }
 }
