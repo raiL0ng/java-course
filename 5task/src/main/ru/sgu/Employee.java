ba4461e0b6981138532768adf508d08af34765ca
raiL0ng <19nik.nikitov.97@mail.ru>
2024-03-10 19:33:48 +0400

    Init

---
+++ 5task/src/main/ru/sgu/Employee.java
@@ -0,0 +1,66 @@
+package main.ru.sgu;
+
+import java.util.Objects;
+
+public class Employee {
+    private String firstName;
+    private String lastName;
+    private String position;
+    private double salary;
+
+    
+    public Employee(String firstName, String lastName, String position, double salary) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.position = position;
+        this.salary = salary;
+    }
+
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+
+    public String getLastName() {
+        return lastName;
+    }
+
+
+    public String getPosition() {
+        return position;
+    }
+
+
+    public double getSalary() {
+        return salary;
+    }
+
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Employee employee = (Employee) obj;
+        return Double.compare(employee.salary, salary) == 0 && Objects.equals(firstName, employee.firstName) &&
+                Objects.equals(lastName, employee.lastName) &&
+                Objects.equals(position, employee.position);
+    }
+
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(firstName, lastName, position, salary);
+    }
+
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", position='" + position + '\'' +
+                ", salary=" + salary +
+                '}';
+    }
+}
